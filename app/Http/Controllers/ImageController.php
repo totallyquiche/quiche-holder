4bea9a6a13108d8dc437441c6907184fedd8e36a
Brian N. Dady <bndady@gmail.com>
2020-12-01 06:44:55 +0000

    Saves images to DB and checks DB for image before calling API

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use AsyncAws\Lambda\LambdaClient;
 
 class ImageController extends Controller
@@ -13,6 +14,34 @@ class ImageController extends Controller
      */
     public function serve(int $width, int $height) : void
     {
+        $image = Image::where('width', $width)
+            ->where('height', $height)
+            ->first();
+
+        if ($image) {
+            $blob = $image->blob;
+        } else {
+            $blob = $this->getImageBlobFromApi($width, $height);
+
+            $this->createImage($width, $height, $blob);
+        }
+
+        header('Content-Type: image/jpg');
+
+        echo $blob;
+
+        exit;
+    }
+
+    /**
+     * Calls the API to get the image blob.
+     *
+     * @param int $width
+     * @param int $height
+     *
+     * @return string
+     */
+    private function getImageBlobFromApi(int $width, int $height) : string {
         $lambda = new LambdaClient([
             'accessKeyId' => env('AWS_LAMBDA_ACCESS_KEY_ID'),
             'accessKeySecret' => env('AWS_LAMBDA_ACCESS_KEY_SECRET'),
@@ -29,10 +58,25 @@ class ImageController extends Controller
             'Payload' => json_encode($payload),
         ]);
 
-        header('Content-Type: image/jpg');
+        return base64_decode($result->getPayload());
+    }
 
-        echo base64_decode($result->getPayload());
+    /**
+     * Create and save a new Image record.
+     *
+     * @param int    $width
+     * @param int    $height
+     * @param string $blob
+     *
+     * @return void
+     */
+    private function createImage(int $width, int $height, string $blob) : void {
+        $image = new Image();
 
-        exit;
+        $image->width = $width;
+        $image->height = $height;
+        $image->blob = $blob;
+
+        $image->save();
     }
 }
---
+++ database/migrations/2020_12_01_054705_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->integer('width');
+            $table->integer('height');
+            $table->index(['width', 'height']);
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE `images` ADD `blob` MEDIUMBLOB');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
